d8139d6fdc8152c4c87c24a0d38185ee380ecaf7
wiculty21 <mahesh.hattika@gmail.com>
2020-12-23 11:16:17 +0530

    updated login.java

--- login.java
+++ login.java
@@ -1,14 +1,12 @@
 class login
 {
-	System.out.println("testing");
-
-	System.out.println("testing");
+	System.out.println("AAAAAAA");	
 	System.out.println("testing");
 	System.out.println("testing");
 	System.out.println("testing testing");
-
-	System.out.println("testing testing");
-	
 	System.out.println("testing testing");
 	System.out.println("testing testing");
+	System.out.println("testing testing");
+	System.out.println("XXXXXXX");
+	System.out.println("YYYYYYY");
 }
